6a3b599dad0c98b24f4f4554c4d9121d658fb383
Paul Michael Reilly <pmr@pajato.com>
2019-07-28 22:13:18 -0400

    Initial commit with working files but no testing or coverage.

---
+++ native/KMP Tutorial App/KMP Tutorial App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KMP Tutorial App
+//
+//  Created by Paul Reilly on 7/28/19.
+//  Copyright © 2019 Paul Reilly. All rights reserved.
+//
+
+import UIKit
+import SharedCode
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
+    }
+
+
+}
+
---
+++ native/KMP Tutorial App/KMP Tutorial AppTests/KMP_Tutorial_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  KMP_Tutorial_AppTests.swift
+//  KMP Tutorial AppTests
+//
+//  Created by Paul Reilly on 7/28/19.
+//  Copyright © 2019 Paul Reilly. All rights reserved.
+//
+
+import XCTest
+@testable import KMP_Tutorial_App
+
+class KMP_Tutorial_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
